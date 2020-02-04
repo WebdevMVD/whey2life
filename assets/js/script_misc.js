bc6263fbbecccccaf3f0b6b93277c0f578c9033c
WebdevMVD <58422562+WebdevMVD@users.noreply.github.com>
2020-02-04 18:12:51 +0000

    04 Feb Mods

---
+++ assets/js/script_misc.js
@@ -0,0 +1,39 @@
+
+
+		function Whey2life_email() {
+
+			var string1 = "tnolan";
+			var string2 = "@";
+			var string3 = "naringtech.ie";
+			var string4 = string1 + string2 + string3;
+			document.write("<a  href=\"" + "mail" + "to:" + string1 + string2 + string3 + "\">" + string4 + "</a>");
+
+
+		}
+
+
+		function Whey2life_email_only() {
+
+			var string1 = "tnolan";
+			var string2 = "@";
+			var string3 = "naringtech.ie";
+			var string4 = string1 + string2 + string3;
+			document.write( string4 );
+
+
+		}
+
+
+		function Whey2life_email_header() {
+
+			var string1 = "tnolan";
+			var string2 = "@";
+			var string3 = "naringtech.ie";
+			var string4 = string1 + string2 + string3;
+			document.write("<a  href=\"" + "mail" + "to:" + string1 + string2 + string3 + "\" class=\"shape\" > <i class=\"fa fa-envelope\"></i>" + string4 + "</a></li>");
+
+
+		}
+
+
+
